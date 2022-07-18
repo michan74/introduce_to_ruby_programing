140e2e09d13977a1d0938d9726767a8c80fa5211
e-sugiura <e-sugiura@wwwave.jp>
2022-07-19 08:22:00 +0900

    運賃計算

--- script/gate.rb
+++ script/gate.rb
@@ -1,13 +1,25 @@
 class Gate
+    STATIONS = [:umeda, :juso, :mikuni]
+    FARES = [160, 190]
+
     def initialize(name)
         @name = name
     end
 
     def enter(ticket)
-
+        ticket.stamp(@name)
     end
 
     def exit(ticket)
-        true
+        fare = calculate_fare(ticket)
+        fare <= ticket.fare
+    end
+
+    def calculate_fare(ticket)
+        enter_station_number = STATIONS.index(ticket.stamped_at)
+        exit_station_number = STATIONS.index(@name)
+
+        difference = (enter_station_number - exit_station_number).abs
+        FARES[difference - 1]
     end
 end
